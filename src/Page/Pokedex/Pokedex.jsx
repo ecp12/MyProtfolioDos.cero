6fbd5e6a4394a42d49d4eb31924398519a97d972
JosephCrespin <erickandrescrespin@gmail.com>
2023-09-06 20:35:53 +0200

    new page Pokedex search

--- src/Page/Pokedex/Pokedex.jsx
+++ src/Page/Pokedex/Pokedex.jsx
@@ -25,7 +25,6 @@ function App() {
       });
   }, []);
 
-  // Función para filtrar los Pokémon basados en el término de búsqueda
   const filteredPokemon = poke.filter((pokemon) =>
     pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -33,39 +32,48 @@ function App() {
   return (
     <div className="Pokedex">
       <Nav />
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search Pokémon..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
-      <div className="pokegallery">
-        {load ? (
-          <p>Loading...</p>
-        ) : (
-          filteredPokemon.map((img, i) => (
-            <div id={img.id} key={img.id}>
-              <div className="cardPoke">
-                <div className="Pokemon">
-                  <img
-                    className="imgPoke"
-                    src={img.sprites.front_default}
-                    alt="pokemon"
-                  />
-                </div>
-                <div className="Pokeball">
-                  <img className="pokeballimg" src={Pokeball} alt="" />
-                </div>
-                <div className="textPoke">
-                  <h5>{img.name}</h5>
-                  <h6>type: {img.types[0].type.name}</h6>
+      <div className="contetPokedex">
+        <div className="tituloPokedex">
+          <h1 className="textContent">Pokedex</h1>
+        </div>
+        <div className="Search">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search Pokémon..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="pokegallery">
+          {load ? (
+            <div class="pokeball"></div>
+          ) : filteredPokemon.length === 0 ? ( // Verifica si no se encontraron resultados
+            <p>No se encontro el pokemon.</p>
+          ) : (
+            filteredPokemon.map((img, i) => (
+              <div id={img.id} key={img.id}>
+                <div className="cardPoke">
+                  <div className="Pokemon">
+                    <img
+                      className="imgPoke"
+                      src={img.sprites.front_default}
+                      alt="pokemon"
+                    />
+                  </div>
+                  <div className="Pokeball">
+                    <img className="pokeballimg" src={Pokeball} alt="" />
+                  </div>
+                  <div className="textPoke">
+                    <h5>{img.name}</h5>
+                    <h6>type: {img.types[0].type.name}</h6>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
-        )}
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
